f9a6aa5460135f510d48da9aff5db3cc5f807f59
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-06-13 15:05:13 +0700

    save

--- backend/middleware/connectsql.js
+++ backend/middleware/connectsql.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const connectionSQL =  mysql.createConnection({
-        host: 'ls-0cb3ed1e8147b51e9e886e9dbc64735b1df5565d.cwl8elctei5a.ap-southeast-1.rds.amazonaws.com',
+        host: '13.212.113.0',
         user: 'root', // <== ระบุให้ถูกต้อง
-        password: '1212312121', // <== ระบุให้ถูกต้อง
-        database: 'dbmaster', // <== ระบุ database ให้ถูกต้อง
+        password: 'mypassword', // <== ระบุให้ถูกต้อง
+        database: 'stock', // <== ระบุ database ให้ถูกต้อง
         port: 3306, // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
     })
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require("body-parser");
 const cors = require("cors")
 const userCtrl = require('./Routes/userCtrl')
+const { connectionSQL } = require("./middleware/connectsql");
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended : false}));
@@ -11,10 +12,26 @@ app.use(bodyParser.json())
 
 app.get("/", async(req,res)=>{
     try {
-        res.json({
-            message : "GOOD"
-        })
+        const connection = await connectionSQL
+        const result = await connection.execute(`select * from user`)
+        if (result) {
+            res.json({
+                message: "API User Already",
+                status: "Successful",
+                data: result[0]
+            }).status(200)
+            await connection.end()
+        } else {
+            res.json({
+                message: "API User Not Already",
+                status: "UnSuccessful"
+
+            }).status(400)
+            await connection.end()
+        }
+        
     } catch (error) {
+        console.log(error);
     }
 })
 
